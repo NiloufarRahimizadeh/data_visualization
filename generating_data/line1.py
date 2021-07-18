1dd185a66a33160b2275839507e9ff538b1730e6
rasulalizadeh <mohammadr.alizadeh@live.com>
2021-07-18 19:03:12 +0430

    27Tir

---
+++ generating_data/line1.py
@@ -0,0 +1,17 @@
+# you’ll need to install it using pip, a module that downloads and installs Python packages.
+# python3 -m pip install --user matplotlib
+import matplotlib.pyplot as plt 
+
+
+squares = [1, 4, 9, 16, 25]
+input_values = [1, 2, 3, 4, 5] 
+fig, ax = plt.subplots() # This function can generate one or more plots in the same figure.
+ax.plot(input_values, squares, linewidth=3) #The variable ax represents a single plot in the figure and is the variable 
+# we’ll use most of the time.
+# We then use the plot() method, which will try to plot the data it’s given in a meaningful way.
+ax.set_title("Square Numbers", fontsize=24)
+ax.set_xlabel("Values", fontsize=14)
+ax.set_ylabel("Square of Value", fontsize=14)
+ax.tick_params(axis="both", labelsize=20)
+plt.show()
+# The function plt.show() opens Matplotlib’s viewer and displays the plot,
---
+++ generating_data/using_style.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+from numpy import square
+
+
+# print(plt.style.available)
+# To use any of these styles, add one line of code before starting 
+# to generate the plot:
+input_values = [1, 2, 3, 4, 5]
+squares = [1, 4, 9, 16, 25]
+plt.plot(input_values, squares)
+plt.style.use("seaborn")
+plt.show()
